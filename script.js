1515a061f6e17db01b3bf2560705502b374e1ffd
therealdoggo <114872108+therealdoggo@users.noreply.github.com>
2022-10-02 12:06:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,167 @@
+
+var funnyNumber = 0;
+var soup = 0;
+function op1() {
+if(soup < 50) {
+document.getElementById("puppy").src="tophat_puppy.png";
+setTimeout(start4,100);
+}
+else{
+setTimeout(undip1,100);
+}
+}
+function op2() {
+if(soup < 50) {
+document.getElementById("puppy").src="tophat_puppy.png";
+setTimeout(start4,100);
+}
+else{
+setTimeout(undip1,100); 
+}
+}
+function op3() {
+if(soup < 50) {
+document.getElementById("puppy").src="tophat_puppy.png";
+setTimeout(start4,100);
+}
+else{
+setTimeout(undip1,100);
+}
+}
+function op4() {
+if(soup < 50) {
+document.getElementById("puppy").src="tophat_puppy.png";
+setTimeout(start4,100);
+}
+else{
+setTimeout(undip1,100);
+}
+}
+function op5() {
+window.location.href = "https://www.youtube.com/watch?v=VbKU58flqH4";
+document.getElementById("puppy").style.display = "none";
+document.getElementById("words").style.display = "block";
+document.getElementById("words").innerHTML = "get mario'ed on";
+}
+function op6() {
+document.getElementById("puppy").src="sophisticated_puppy.png";
+setTimeout(start6,200);
+}
+function op7() {
+document.getElementById("puppy").src="soup.png";
+undip1();
+undip2();
+document.getElementById("btn1").innerHTML = "soup";
+document.getElementById("btn2").innerHTML = "soup";
+document.getElementById("btn3").innerHTML = "soup";
+document.getElementById("btn4").innerHTML = "soup";
+document.getElementById("btn5").innerHTML = "soup";
+document.getElementById("btn6").innerHTML = "soup";
+document.getElementById("btn7").innerHTML = "soup";
+document.getElementById("btn8").innerHTML = "soup";
+document.getElementById("words").style.display = "block";
+document.getElementById("words").innerHTML = "soup";
+soup = soup + 69;
+}
+function op8() {
+document.getElementById("puppy").style.display = "none";
+dip1();
+dip2();
+document.getElementById("words").style.display = "block";
+document.getElementById("words").innerHTML = "dies of cringe";
+}
+function start6() {
+window.alert("I percieve your sophistication, fellow enjoyer of the casserole. You may extract yourself from the vicinity with freedom of disturbance.");
+window.alert("jk");
+window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+function start5() {
+window.alert("Out of pure curiosity, of the world's finest delicacies, which would you partake in foremost?");
+undip2();
+document.getElementById("btn5").innerHTML = "caviar";
+document.getElementById("btn6").innerHTML = "casserole";
+document.getElementById("btn7").innerHTML = "soup";
+document.getElementById("btn8").innerHTML = "beetroot & blackberry cured salmon grilled on a slow flame served on crackers as a canape";
+}
+function start4() {
+window.alert("All is well, I forgive you for your prior misdeeds.");
+setTimeout(start5,1000);
+}  
+function start3() {
+window.alert("maybe click the buttons under me instead?")
+undip1();
+funnyNumber = funnyNumber + 169;
+document.getElementById("btn1").innerHTML = "by all means";
+document.getElementById("btn2").innerHTML = "in the affirmative";
+document.getElementById("btn3").innerHTML = "beyond a doubt";
+document.getElementById("btn4").innerHTML = "without fail";
+}
+function dip1() {
+document.getElementById("btn1").style.display = "none";
+document.getElementById("btn2").style.display = "none";
+document.getElementById("btn3").style.display = "none";
+document.getElementById("btn4").style.display = "none";
+}
+function undip1() {
+document.getElementById("btn1").style.display = "block";
+document.getElementById("btn2").style.display = "block";
+document.getElementById("btn3").style.display = "block";
+document.getElementById("btn4").style.display = "block";
+}
+function dip2() {
+    document.getElementById("btn5").style.display = "none";
+    document.getElementById("btn6").style.display = "none";
+    document.getElementById("btn7").style.display = "none";
+    document.getElementById("btn8").style.display = "none";
+    }
+    function undip2() {
+    document.getElementById("btn5").style.display = "block";
+    document.getElementById("btn6").style.display = "block";
+    document.getElementById("btn7").style.display = "block";
+    document.getElementById("btn8").style.display = "block";
+    }
+
+function start2() {
+window.alert("meanie >:(");
+setTimeout(start3, 2000);
+
+}
+function start1() {
+dip1();
+dip2();
+window.alert("Why did you click me? that hurt :(")
+setTimeout(start2, 1000);
+}
+function funnyThing() {
+if(funnyNumber == 0) {
+document.getElementById("puppy").src="angy_puppy.png";
+funnyNumber = funnyNumber + 1;
+setTimeout(start1, 250);
+}
+if(100 > funnyNumber > 0) {
+console.log(funnyNumber);
+}
+else {
+dip1();
+dip2();
+document.getElementById("puppy").style.display = "none";
+document.getElementById("words").style.display = "block";
+document.getElementById("words").innerHTML = "he died of click";
+document.getElementById("eggdiv").style.display = "block";
+}
+}
+
+function idol() {
+window.location.href = "https://www.youtube.com/shorts/aCgP8BFjrw4";
+}
+// <img id="puppy" src="puppy.png" onclick="funnyThing()">
+//    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+
+
+//dip1();
+//dip2();
+//document.getElementById("puppy").style.display = "none";
+//document.getElementById("words").style.display = "block";
+//document.getElementById("words").innerHTML = "he died of click";
+//document.getElementById("eggdiv").style.display = "block";
